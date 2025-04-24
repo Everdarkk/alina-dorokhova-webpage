b03fa3e74dd28ab783ab0a0b2159a55b1db123e1
Oleksandr Dorokhov <alex.dorokhov.mail@gmail.com>
2025-04-24 18:14:10 +0300

    Metadata changing...

--- src/components/Metadata.tsx
+++ src/components/Metadata.tsx
@@ -3,6 +3,7 @@ import Head from "next/head"
 export default function Metadata() {
     return (
         <Head>
+            <meta name="title" content="Аліна Дорохова | Логопед"/>
             <meta name="description" content="Сайт логопеда Аліни Дорохової. Онлайн-консультації, корекція мовлення, розвиток мовлення у дітей." />
             <meta name="keywords" content="логопед, логопед онлайн, логопед для дітей, корекція мовлення, розвиток мовлення, консультації логопеда, онлайн консультації, логопед для дорослих, логопед для підлітків, логопед для дітей з аутизмом, логопед для дітей з дислексією, логопед для дітей з затримкою мовлення, логопед для дітей з фонетичними порушеннями, логопед для дітей з дизартрією, логопед для дітей з алалією" />
             <meta name="creator" content="Oleksandr Dorokhov" />
@@ -12,7 +13,7 @@ export default function Metadata() {
             <meta property="og:image" content="" />
             <meta property="og:locale" content="uk_UA" />
             <meta property="og:type" content="website" />
-            <meta property="og:url" content=""/>
+            <meta property="og:url" content="https://alina-dorokhova.vercel.app/"/>
         </Head>
     )
 }
